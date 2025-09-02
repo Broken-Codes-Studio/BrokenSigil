9bd4f0bd8396a4f13d5434dc44b470ea49b17538
Ghost Alireza <alireza_ka@outlook.com>
2025-09-02 22:00:05 +0330

    Updated IPriority.cs

--- Scripts/Interface/IPriority.cs
+++ Scripts/Interface/IPriority.cs
@@ -10,5 +10,5 @@ public interface IPriority<T> where T : ISignedNumber<T>
     /// <summary>
     /// Gets or sets the priority of the object.
     /// </summary>
-    public T Priority { get; set; }
+    public T Priority { get;}
 }
