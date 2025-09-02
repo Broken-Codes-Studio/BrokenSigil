c547b4afe8be864ad86c33d3ee0da3c8e3598502
Ghost Alireza <alireza_ka@outlook.com>
2025-09-02 21:59:34 +0330

    Added priority to the slots

--- Scripts/ModificationSystem/Kits/ModificationSlot.cs
+++ Scripts/ModificationSystem/Kits/ModificationSlot.cs
@@ -2,20 +2,25 @@ namespace BrokenSigilCollection.Modification;
 
 using Godot;
 
-public abstract class Slot
+using BrokenSigilCollection.Interface;
+
+public abstract class Slot : IPriority<sbyte>
 {
 
     public readonly StringName Name;
     public readonly StringName[] Dependencies;
+    public readonly bool Main;
 
     public bool Filled { get; set; }
-    public bool Main { get; private set; }
+    public sbyte Priority { get; set; } = 0;
 
-    public Slot(StringName name, StringName[] dependencies, bool main)
+    public Slot(StringName name, StringName[] dependencies, bool main, sbyte priority = 0)
     {
         this.Name = name;
         this.Dependencies = dependencies;
         this.Main = main;
+
+        this.Priority = priority;
     }
 }
 
@@ -23,7 +28,7 @@ public class Slot3D : Slot
 {
     public readonly Vector3 Position;
 
-    public Slot3D(StringName name, StringName[] dependencies, Vector3 position, bool main) : base(name, dependencies, main)
+    public Slot3D(StringName name, StringName[] dependencies, Vector3 position, bool main, sbyte priority = 0) : base(name, dependencies, main, priority)
     {
         this.Position = position;
     }
@@ -33,7 +38,7 @@ public class Slot2D : Slot
 {
     public readonly Vector2 Position;
 
-    public Slot2D(StringName name, StringName[] dependencies, Vector2 position, bool main) : base(name, dependencies, main)
+    public Slot2D(StringName name, StringName[] dependencies, Vector2 position, bool main, sbyte priority = 0) : base(name, dependencies, main, priority)
     {
         this.Position = position;
     }
